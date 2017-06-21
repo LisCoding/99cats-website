265d0a09d603415f55a0d64bcb85194c1ab8f704
Liseth Cardozo <cardozoliseth@gmail.com>
2017-06-21 16:51:40 -0700

    Add custom validation

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -1,14 +1,35 @@
 class CatRentalRequest < ApplicationRecord
   validates :cat_id, :start_date, :end_date, presence: true
   validates :status, presence: true, inclusion: %w(PENDING APPROVED DENIED)
-
-  belongs_to :cat, 
+  validates :does_not_overlap_aproved_request
+  
+  belongs_to :cat,
     primary_key: :id,
     foreign_key: :cat_id,
     class_name: :Cat
 
-  # def overlapping_requests
-  #
-  # end
+  def overlapping_requests
+    CatRentalRequest
+      .where.not(id: self.id)
+      .where(cat_id: cat_id)
+      .where(<<-SQL, start_date: start_date, end_date: end_date)
+        NOT((start_date > :end_date) OR (end_date < :start_date))
+      SQL
+
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.where("status = APPROVED")
+  end
+
+  def overlapping_pending_requests
+    overlapping_requests.where("status = PENDING")
+  end
+
+  def does_not_overlap_aproved_request
+    return if self.denied?
+    unless overlapping_approved_requests.empty?
+      error[:base] << "Sorry, Cat is already taken"
+  end
 
 end
